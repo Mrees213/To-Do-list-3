7aa209be7b01de7a615a0efdc1bba0645227b57b
Morgan Rees <morgan.w.rees@gmail.com>
2021-02-08 10:48:50 -0600

    Initial Commit

---
+++ To Do list 3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do list 3
+//
+//  Created by Morgan w Rees on 2/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To Do list 3/To_Do_list_3App.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_list_3App.swift
+//  To Do list 3
+//
+//  Created by Morgan w Rees on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_list_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
